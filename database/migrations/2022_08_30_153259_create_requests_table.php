980346ba790264a4913379d22a7d213f5ccbe46c
grahamkatana <graykatanakenny@gmail.com>
2022-08-30 17:44:53 +0200

    Migrations and models

---
+++ database/migrations/2022_08_30_153259_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('otp')->unique();
+            $table->boolean('is_valid')->default(true);
+            $table->string('expires_in')->default(date("m/d/Y h:i:s a",strtotime("+30 seconds")));
+            $table->foreignId('requester_id')->references('id')->on('requesters')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
